e6dd7a5a267da136adbe295d5ac5f8b12ddab975
mstamos <mstamos@gmail.com>
2018-01-12 23:02:48 +0200

    Built counter app

--- counter/src/index.js
+++ counter/src/index.js
@@ -1,10 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class HelloMessage extends React.Component {
+class Counter extends React.Component {
+  constructor() {
+    super();
+    this.state ={
+      counter: 0
+    };
+
+    this.increaseCounter = this.increaseCounter.bind(this);
+    this.decreaseCounter = this.decreaseCounter.bind(this);
+  }
+
+  increaseCounter() {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  }
+
+  decreaseCounter() {
+    this.setState({
+      counter: this.state.counter - 1
+    });
+  }
+
    render () {
-     return <div>Hello {this.props.name}</div>;
+    return (
+      <div>
+        Counter: {this.state.counter}
+        <p></p>
+        <button onClick={this.increaseCounter}> + </button>
+        <button onClick={this.decreaseCounter}> - </button>
+      </div>
+     );
    }
 }
 
-ReactDOM.render(<HelloMessage name='John'/>, document.getElementById('root'));
+ReactDOM.render(<Counter/>, document.body);
