110bb1a2c0f12d2deffa8278217e2ab21b465a65
mstamos <mstamos@gmail.com>
2018-01-12 23:35:47 +0200

    Connect two logins to share the same state

--- two-logins/src/App.js
+++ two-logins/src/App.js
@@ -2,23 +2,71 @@ import React from 'react';
 import './App.css';
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      username: '',
+      password: ''
+    }
+
+    this.onUsernameChange = this.onUsernameChange.bind(this);
+    this.onPasswordChange = this.onPasswordChange.bind(this);
+  }
+
+  onUsernameChange(event) {
+    this.setState({
+      username: event.target.value
+    });
+  }
+
+  onPasswordChange(event) {
+    this.setState({
+      password: event.target.value
+    });
+  }
+
   render() {
     return (
       <div className='container'>
         <div className='right-top-login'>
-          <input type="text" placeholder="Enter Username" name="uname"/>
+          <input
+            value={this.state.username}
+            onChange={this.onUsernameChange}
+            type="text"
+            placeholder="Enter Username"
+            name="uname"
+          />
           <span className='right-space'/>
-          <input type="password" placeholder="Enter Password" name="psw"/>
+          <input
+            value={this.state.password}
+            onChange={this.onPasswordChange}
+            type="password"
+            placeholder="Enter Password"
+            name="psw"
+          />
           <span className='right-space'/>
           <button>Submit</button>
         </div>
 
         <div className='main-login'>
           <p>
-            <input type="text" placeholder="Enter Username" name="uname"/>
+            <input
+              value={this.state.username}
+              onChange={this.onUsernameChange}
+              type="text"
+              placeholder="Enter Username"
+              name="uname"
+            />
           </p>
           <p>
-            <input type="password" placeholder="Enter Password" name="psw"/>
+            <input
+              value={this.state.password}
+              onChange={this.onPasswordChange}
+              type="password"
+              placeholder="Enter Password"
+              name="psw"
+            />
           </p>
           <button>Submit</button>
         </div>
