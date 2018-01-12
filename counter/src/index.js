e1f77413196366de0217d12075002f0bbe68a389
mstamos <mstamos@gmail.com>
2018-01-12 23:01:52 +0200

    Display the name which passed from props

--- counter/src/index.js
+++ counter/src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
 class HelloMessage extends React.Component {
    render () {
-     return <div>Hello World</div>;
+     return <div>Hello {this.props.name}</div>;
    }
 }
 
-ReactDOM.render(<HelloMessage />, document.getElementById('root'));
+ReactDOM.render(<HelloMessage name='John'/>, document.getElementById('root'));
