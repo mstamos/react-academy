3ea8f2f097920b46c3525b7f460df41b844d3076
mstamos <mstamos@gmail.com>
2018-01-13 21:03:01 +0200

    Display artist names

--- artists/src/artists.js
+++ artists/src/artists.js
@@ -6,20 +6,38 @@ class Artists extends React.Component {
     this.state= {
       artists: []
     };
+
+    this.displayArtists = this.displayArtists.bind(this);
+  }
+
+  displayArtists() {
+    return (
+      <ul>
+        {this.state.artists.map((artist, index) => {
+          return <li key={index}>
+            {artist.name}
+            <button>Remove</button>
+           </li>
+        })}
+      </ul>
+    )
   }
 
   componentDidMount() {
     fetch('http://localhost:3001/artists')
       .then(res => res.json())
-      .then(data => {
-        console.log(data)
-      })
+      .then(artists => {
+        this.setState({
+          artists
+        });
+      });
   }
 
   render() {
     return (
       <div>
         Artists
+        {this.displayArtists()}
       </div>
     )
 
