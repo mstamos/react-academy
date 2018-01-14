bf2c4e9fe65eb43517065192dc300e8fca0d3fed
mstamos <mstamos@gmail.com>
2018-01-14 09:29:56 +0200

    Add food data

--- searching-food/src/index.js
+++ searching-food/src/index.js
@@ -1,6 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './app';
 
-const App = () => 'React Academy';
+const foods = [
+  {
+    name: 'mosxari',
+    portions: 10
+  },
+  {
+    name: 'fasolada',
+    portions: 0
+  },
+  {
+    name: 'tzatziki',
+    portions: 15
+  },
+  {
+    name: 'mousakas',
+    portions: 0
+  },
+  {
+    name: 'fasolakia',
+    portions: 8
+  }
+];
 
 ReactDOM.render(<App />, document.getElementById('root'));
