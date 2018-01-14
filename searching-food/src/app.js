f2c5218b39b542bea3e6b8bbbf5199aa8e925c17
mstamos <mstamos@gmail.com>
2018-01-14 10:01:22 +0200

    Add searching functionality on food items

--- searching-food/src/app.js
+++ searching-food/src/app.js
@@ -1,20 +1,43 @@
 import React from 'react';
 
 class App extends React.Component {
-  constructor() {
+  constructor(props) {
     super();
 
+    this.state ={
+      foods: props.foods
+    };
+
     this.displayFoods = this.displayFoods.bind(this);
+    this.filterFoods = this.filterFoods.bind(this);
   }
 
   displayFoods() {
-    return this.props.foods.map((food, index) => {
+    return this.state.foods.map((food, index) => {
       return <li key={index}>{food.name}</li>;
     })
   }
+
+  filterFoods(event) {
+    const value = event.target.value;
+
+    const filteredFoods = this.props.foods.filter(food => {
+      return food.name.includes(value);
+    });
+
+    this.setState({
+      foods: filteredFoods
+    })
+  }
+
   render() {
     return (
       <div>
+        <input
+          type='text'
+          placeholder='Search for Food'
+          onChange={this.filterFoods}
+        />
         <ul>
           {this.displayFoods()}
         </ul>
