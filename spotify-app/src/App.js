3c816a666bd407e244a7cb6edb7a8907ba773e67
mstamos <mstamos@gmail.com>
2018-01-19 20:23:54 +0200

    Add top menu bar

--- spotify-app/src/App.js
+++ spotify-app/src/App.js
@@ -5,6 +5,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <ul>
+          <li><a className="active" href="#home">Home</a></li>
+          <li><a href="#news">Favorite</a></li>
+        </ul>
+
         Let get the party started!
       </div>
     );
