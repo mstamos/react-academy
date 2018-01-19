28e8cb50c6273fc7593aac1bb87569bd565e8a6e
mstamos <mstamos@gmail.com>
2018-01-19 20:21:34 +0200

    Bootstrap app
     Add
      - README file with images
      - Initial css
      - db.json

---
+++ spotify-app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Let get the party started!
+      </div>
+    );
+  }
+}
+
+export default App;
